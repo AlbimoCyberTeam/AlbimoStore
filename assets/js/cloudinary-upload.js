f3b0cef8de7c0c7f1722ff99a7b1641aae393de1
HR-XXII <betazona1984@gmail.com>
2026-05-31 04:54:34 +0900

    Update cloudinary-upload.js

--- assets/js/cloudinary-upload.js
+++ assets/js/cloudinary-upload.js
@@ -1,18 +1,27 @@
-const CLOUDINARY_URL = "https://cloudinary.com";
+const CLOUDINARY_URL =
+  "https://api.cloudinary.com/v1_1/albimostore/image/upload";
+
 const CLOUDINARY_UPLOAD_PRESET = "albimostore";
 
 export async function uploadImage(file) {
   const formData = new FormData();
+
   formData.append("file", file);
   formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
 
   try {
     const response = await fetch(CLOUDINARY_URL, {
       method: "POST",
-      body: formData,
+      body: formData
     });
+
     const data = await response.json();
-    return data.secure_url; // Mengembalikan URL gambar siap simpan di Firebase
+
+    if (!response.ok) {
+      throw new Error(data.error?.message || "Upload gagal");
+    }
+
+    return data.secure_url;
   } catch (error) {
     console.error("Cloudinary Upload Error:", error);
     throw error;
