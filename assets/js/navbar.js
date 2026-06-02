f485d83209d3ac72bb90a9e3d466d3ba3afe8d11
HR-XXII <betazona1984@gmail.com>
2026-06-02 22:26:34 +0900

    Add files via upload

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,4 +1,3 @@
-// assets/js/navbar.js
 import { auth, db } from "../../firebase/config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
