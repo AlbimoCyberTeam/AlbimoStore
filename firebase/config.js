19c37270867a9c184cc828d7661267987343fcc1
HR-XXII <betazona1984@gmail.com>
2026-06-02 16:22:19 +0900

    Update Firebase configuration with new credentials

--- firebase/config.js
+++ firebase/config.js
@@ -5,12 +5,12 @@ import { getFirestore } from "https://gstatic.com";
 
 // Ganti dengan konfigurasi asli dari Firebase Console Anda
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "://firebaseapp.com",
-    projectId: "your-app",
-    storageBucket: "://appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyC_mlSh_6OoqgydUbfhFPpXEBAQjcBNNiM",
+    authDomain: "albimostore.firebaseapp.com",
+    projectId: "albimostore",
+    storageBucket: "albimostore.firebasestorage.app",
+    messagingSenderId: "609426144482",
+    appId: "1:609426144482:web:fa6c9107832ea6ff4d72bb"
 };
 
 // Inisialisasi Firebase
@@ -20,6 +20,6 @@ export const db = getFirestore(app);
 
 // Konfigurasi Cloudinary (untuk upload gambar)
 export const cloudinaryConfig = {
-    cloudName: "YOUR_CLOUDINARY_NAME",
-    uploadPreset: "YOUR_UPLOAD_PRESET" // Pastikan mode 'unsigned' aktif di Cloudinary
+    cloudName: "diutiwqz1",
+    uploadPreset: "albimostore" // Pastikan mode 'unsigned' aktif di Cloudinary
 };
